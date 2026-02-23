f59664620d7ea83fcff8b5d2f88be83443d184db
Jorge Luiz <luiz.bevilacquam@gmail.com>
2026-02-23 16:10:28 -0400

    Ex004 finished

---
+++ Exercicios/ex004/scripts/script.js
@@ -0,0 +1,31 @@
+
+var teste = 1;
+
+nome = "João";
+
+console.log(teste);
+
+teste = 'Matheus';
+
+console.log(teste);
+console.log(nome);
+
+var $nome = 'asd';
+var _nome = 'asd2';
+
+console.log($nome);
+console.log(_nome);
+
+var nome5 = 'asd3';
+
+console.log(nome5);
+
+var meuPrimeiroNome = 'Jorge'; // camelCase
+
+console.log(meuPrimeiroNome)
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
