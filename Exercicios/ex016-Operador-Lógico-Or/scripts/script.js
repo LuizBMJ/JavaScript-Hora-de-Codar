6ebaf2c8f190c73fff433a28b3d9968a82ead21c
Jorge Luiz <luiz.bevilacquam@gmail.com>
2026-04-28 19:11:35 -0400

    Some Js exercises

---
+++ Exercicios/ex016-Operador-Lógico-Or/scripts/script.js
@@ -0,0 +1,15 @@
+var idade = 16;
+var nome = "João";
+
+
+if(nome == "João" || idade >= 18) {
+    console.log("O joão pode entrar na aula de esgrima")
+} else {
+    console.log("Este não é o joão ou ele é menor de idade, ele não pode entrar na aula de esgrima")
+}
+
+if(nome == "Pedro" && 15 > 20 || 10 == 10) {
+    console.log("Condições combinadas são verdadeiras")
+} else {
+    console.log("Condições combinadas são falsas")
+}
---
+++ Exercicios/ex017-Operador-Lógico-Not/scripts/script.js
@@ -0,0 +1,11 @@
+if(!true){
+    console.log("Esta condição é falsa")
+}
+
+var nome = "A";
+
+if(!(nome == "João")) {
+    console.log("O nome não é João")
+} else {
+    console.log("O nome é João")
+}
